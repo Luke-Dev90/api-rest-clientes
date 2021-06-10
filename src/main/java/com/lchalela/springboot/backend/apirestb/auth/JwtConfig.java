17e3e316fc927065db13c5411839fe72731e81b5
Luke-Dev90 <lukee_sf@hotmail.com>
2021-06-10 17:10:47 -0300

    Fixed

--- src/main/java/com/lchalela/springboot/backend/apirestb/auth/JwtConfig.java
+++ src/main/java/com/lchalela/springboot/backend/apirestb/auth/JwtConfig.java
@@ -4,4 +4,5 @@ public class JwtConfig {
 	public static final String LLAVE_SECRETA ="alguna.clave.secreta.12345678";
 	public static final String RSA_PRIVATE = "";
 	public static final String RSA_PUBLICA = "";
+	
 }
